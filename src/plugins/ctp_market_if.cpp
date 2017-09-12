415fb286b9b5da1d96aa917f7a17524efb4bdfe2
yon2kong <yon2kong@gmail.com>
2017-09-13 05:31:08 +0800

    fix bug of not fixing ctp date during non trading time

--- src/plugins/ctp_market_if.cpp
+++ src/plugins/ctp_market_if.cpp
@@ -199,14 +199,20 @@ void market_if::OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarke
 	time_t utc_time = time(NULL);
 
 	// fix remote date
-	if ((remote_ptime.time_of_day() >= time_duration(8,59,0) &&
-	     remote_ptime.time_of_day() <= time_duration(15,0,0)) ||
-	    remote_ptime.time_of_day() >= time_duration(20,59,0) ||
-	    remote_ptime.time_of_day() <= time_duration(2,30,0)) {
-		while (remote_time + 1800 < utc_time) // 1800 other than 3600
-			remote_time += 3600;
-		while (remote_time - 1800 > utc_time)
-			remote_time -= 3600;
+	//if ((remote_ptime.time_of_day() >= time_duration(8,59,0) &&
+	//     remote_ptime.time_of_day() <= time_duration(15,0,0)) ||
+	//    remote_ptime.time_of_day() >= time_duration(20,59,0) ||
+	//    remote_ptime.time_of_day() <= time_duration(2,30,0)) {
+	//	while (remote_time + 1800 < utc_time) // 1800 other than 3600
+	//		remote_time += 3600;
+	//	while (remote_time - 1800 > utc_time)
+	//		remote_time -= 3600;
+	//} else {
+	//	remote_time = utc_time - utc_time % 86400 + remote_time % 86400;
+	//}
+	if (remote_time / 86400 != utc_time / 86400 &&
+	    (remote_time + 1800 < utc_time || remote_time - 1800 > utc_time)) {
+		remote_time = utc_time - utc_time % 86400 + remote_time % 86400;
 	}
 
 	// fix remote time
